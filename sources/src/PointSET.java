f2623167a3942d302f9c12db32293516e1ed29c3
MogilnikovAB <amogilnikov@oaorti.ru>
2017-07-24 17:20:20 +0300

    [add] Templates for classes [add] Tests

---
+++ sources/src/PointSET.java
@@ -0,0 +1,44 @@
+/**
+ * Created by orps on 24.07.17.
+ */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+
+public class PointSET {
+
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+
+    }
+
+    public Point2D nearest(Point2D p){
+        return new Point2D(0, 0);
+    }
+
+    public static void main(String[] args) {}
+}
